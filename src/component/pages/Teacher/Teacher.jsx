42b80da48fa66561e174d144aaa2cbb1d37f95f3
sara-likaount <saralikaunt@gmail.com>
2022-08-30 21:38:55 +0300

    n

--- src/component/pages/Teacher/Teacher.jsx
+++ src/component/pages/Teacher/Teacher.jsx
@@ -1,17 +1,23 @@
 import "./Teacher.css";
 import React, { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
 function Teacher() {
-  const [teachers, setTeachers]=useState("");
+  const [teachers, setTeachers]= useState([]);
 
     useEffect(() => {
       fetch("http://localhost:8000/KinderGarten")
       .then(res => {
         return res.json()})
         .then((data)=>{
-            console.log(data[0][0]);
-            // setTeachers(data[0][0])
+            console.log(data);
+            setTeachers(data);
         })
       },[]);
 
@@ -19,10 +25,23 @@ function Teacher() {
 
   return (
     <div className="teacher">
-      <h1>{teachers}</h1>
+      <h1>{teachers.firstname}</h1>
+
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          name+lastname
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          phone
+        </Typography>
+      </CardContent>
+    </Card>
 
     </div>
   );
 };
 
 export default Teacher;
+
+
