3b61602cb414ebd486b3f499a8e4a6f25bb0f735
sara-likaount <saralikaunt@gmail.com>
2022-08-30 19:03:49 +0300

    n

--- src/component/pages/Teacher/Teacher.jsx
+++ src/component/pages/Teacher/Teacher.jsx
@@ -1,10 +1,26 @@
 import "./Teacher.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 
 function Teacher() {
+  const [teachers, setTeachers]=useState("");
+
+    useEffect(() => {
+      fetch("http://localhost:8000/KinderGarten")
+      .then(res => {
+        return res.json()})
+        .then((data)=>{
+            console.log(data[0][0]);
+            // setTeachers(data[0][0])
+        })
+      },[]);
+
+
+
   return (
     <div className="teacher">
-      <h1>Teacher</h1>
+      <h1>{teachers}</h1>
+
     </div>
   );
 };
